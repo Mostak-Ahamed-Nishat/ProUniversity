c3a04ffba1b9d878d44c5ea1412ea182f1a951c5
Mostak Ahamed Nishat <mostakahamed484@gmail.com>
2025-07-11 10:50:50 +0600

    User validation updated

--- src/modules/user/user.validation.ts
+++ src/modules/user/user.validation.ts
@@ -1,14 +1,10 @@
 import { z } from "zod";
 
 const userSchemaValidation = z.object({
-  id: z.string(),
   password: z
-    .string()
-    .max(20, { message: "Password can't be more than 20 characters" }),
-  role: z.enum(["admin", "faculty", "student"]),
-  needPasswordChange: z.boolean().optional(),
-  status: z.enum(["in-progress", "blocked"]).default("in-progress"),
-  isDeleted: z.boolean().default(false).optional(),
+    .string({ invalid_type_error: "Password must be character with number" })
+    .max(20, { message: "Password can't be more than 20 characters" })
+    .optional(),
 });
 
 export const UserValidation = { userSchemaValidation };
